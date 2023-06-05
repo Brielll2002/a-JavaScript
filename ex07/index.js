eaad4784a2f18de191dfc756148ade46c611e0af
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2023-06-05 15:37:51 -0300

    if && else

---
+++ ex07/index.js
@@ -0,0 +1,4 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){console.log('Passou da velocidade permitida. Multado !!')}
+console.log('Dirija sempre com responsabilidade !')
