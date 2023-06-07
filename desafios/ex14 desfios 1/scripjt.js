e56eff6ca0301c0005d5ac07d1e8e008b3ab08d5
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2023-06-07 16:06:55 -0300

    treinando JS
    Horas Do Dia

---
+++ desafios/ex14 desfios 1/scripjt.js
@@ -0,0 +1,17 @@
+function carregar(){var msg = document.getElementById('msg')
+var img = document.getElementById('manha')
+var data = new Date()
+var hora = data.getHours()
+//var hora = '22'
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if(hora >= 0 && hora <= 12){img.src = 'manha2.jpeg'
+document.body.style.background = '#87cefa'
+msg2.innerHTML = 'Bom Dia!'}
+else if(hora >= 12 && hora <= 18){img.src = 'tarde2.jpeg'
+document.body.style.background = '#f87021'
+msg2.innerHTML = 'Boa Tarde!'}
+else if(hora > 18){img.src = 'noite2.jpeg'
+document.body.style.background = '#110a24'
+msg2.innerHTML = 'Boa Noite!'}}
+
